93e472564214b31a741073f8a3f5c4f22cbc942b
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-09-20 17:48:41 +0700

    styling:hero section and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,37 +7,51 @@ import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center container flex-1 w-full text-center">
-      <Navbar />
-      <DarkMode />
+    <main className="flex flex-col items-center justify-center flex-1 w-full text-center max-w-[1440px] mx-auto">
+      <nav className="flex justify-center w-full items-center z-50">
+        <DarkMode />
+        <Navbar />
+      </nav>
 
-      <div className="w-full h-screen">
+      <div className="w-full h-screen relative">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ ease: 'easeOut', duration: 2 }}
+          transition={{ ease: 'easeOut', duration: 1 }}
           className="flex flex-col w-full justify-center"
         >
           <Image
             src="/landing-page/hero-flufxy.png"
             alt="hero-image"
-            className="w-auto mt-10 h-auto object-cover z-10"
+            className="w-auto h-auto mt-10 object-cover z-10"
             width={1000}
             height={500}
             quality={100}
           />
         </motion.div>
 
-        {/* <motion.h1
+        <div>
+          <motion.h1
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ ease: 'easeOut', duration: 1, delay: 1 }}
-            className="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[100px] font-bold font-balo"
+            className="absolute top-5 lg:top-10 h-full w-full mx-auto text-[32px] lg:text-[90px] xl:text-[100px] font-bold font-balo"
           >
-            SOUFFLE CAKE & BAKERY
-          </motion.h1> */}
+            FLUFFY & MELTED <br /> WITH
+          </motion.h1>
+
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ ease: 'easeOut', duration: 1, delay: 1.5 }}
+            className="absolute top-[130px] lg:top-1/2 left-1/2 z-10 transform -translate-x-1/2 -translate-y-1/2 text-[32px] lg:text-[90px] xl:text-[100px] font-bold font-balo"
+          >
+            FLUFXY
+          </motion.h1>
+        </div>
       </div>
     </main>
   );
