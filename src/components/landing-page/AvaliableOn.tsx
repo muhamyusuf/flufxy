0a34755df7a26b6320a7291d0dd282557c6e80d4
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-09-30 21:01:18 +0700

    add text soon on avaliable on

--- src/components/landing-page/AvaliableOn.tsx
+++ src/components/landing-page/AvaliableOn.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import Image from 'next/image';
 
 export default function AvaliableOn() {
@@ -7,13 +9,17 @@ export default function AvaliableOn() {
         avaliable on
       </h2>
 
-      <div className="flex-wrap flex gap-2 md:gap-10 justify-center items-center dark:bg-white rounded-lg">
+      <div className="flex-wrap flex gap-2 md:gap-10 justify-center items-center dark:bg-white/90 overflow-hidden rounded-lg relative">
+        <p className="absolute inset-0 text-black flex justify-center w-full h-full text-2xl items-center font-extrabold z-20">
+          Soon
+        </p>
+
         <Image
           src="/avaliable-on/gofood.svg"
           alt="gofood logo"
           width={100}
           height={100}
-          className="aspect-3/2 w-[120px] md:w-[150px] lg:w-[200px] scale-50 object-contain bg-blend-color-burn rounded-sm"
+          className="aspect-3/2 w-[120px] md:w-[150px] lg:w-[200px] scale-50 object-contain bg-blend-color-burn rounded-sm opacity-50"
         />
 
         <Image
@@ -21,7 +27,7 @@ export default function AvaliableOn() {
           alt="grab logo"
           width={100}
           height={100}
-          className="aspect-3/2 w-[120px] md:w-[150px] lg:w-[200px] scale-150 object-contain bg-blend-color-burn rounded-sm"
+          className="aspect-3/2 w-[120px] md:w-[150px] lg:w-[200px] scale-150 object-contain bg-blend-color-burn rounded-sm opacity-50"
         />
 
         <Image
@@ -29,7 +35,7 @@ export default function AvaliableOn() {
           alt="shopee logo"
           width={100}
           height={100}
-          className="aspect-3/2 w-[120px] md:w-[150px] lg:w-[200px] object-cover bg-blend-color-burn rounded-sm"
+          className="aspect-3/2 w-[120px] md:w-[150px] lg:w-[200px] object-cover bg-blend-color-burn rounded-sm opacity-50"
         />
 
         <Image
@@ -37,7 +43,7 @@ export default function AvaliableOn() {
           alt="maxim logo"
           width={100}
           height={100}
-          className="aspect-3/2 w-[120px] md:w-[150px] lg:w-[200px] scale-50 object-contain bg-blend-color-burn rounded-xl"
+          className="aspect-3/2 w-[120px] md:w-[150px] lg:w-[200px] scale-50 object-contain bg-blend-color-burn rounded-xl opacity-50"
         />
       </div>
     </div>
